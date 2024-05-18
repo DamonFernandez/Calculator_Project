027e50da8a04774a009f06c69eb72b37c36d01e7
Damon Fernandez <damonfernandez04@gmail.com>
2024-05-18 15:33:14 -0400

    remove custom arithmetic functions as they weren't needed with new design

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,3 @@
-let add = (...numbers) => {
-  let sum = 0;
-  numbers.forEach((element) => {
-    sum += element;
-  });
-  return sum;
-};
-
-let subtract = (...numbers) => {
-  let difference = 0;
-  numbers.forEach((element) => {
-    difference -= element;
-  });
-  return difference;
-};
-
-let multiply = (...numbers) => {
-  let product = 0;
-  numbers.forEach((element) => {
-    product *= element;
-  });
-  return product;
-};
-
-let divide = (...numbers) => {
-  let quotient = 0;
-  numbers.forEach((element) => {
-    quotient -= element;
-  });
-  return quotient;
-};
-
 let performOperation = (operationType, operandOne = 0, operandTwo = 0) => {
   switch (operationType) {
     case "+":
